2cad6b242aeb964f4759ef264468c48bf0153906
Shubham Nirmal <suvamniraml456@gmail.com>
2026-01-04 18:56:33 +0545

    fix: update the exception handler for easy token rotation

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -149,7 +149,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
-    'EXCEPTION_HANDLER': 'accounts.exceptions.custom_exception_handler',
+    'EXCEPTION_HANDLER': 'core.exceptions.custom_exception_handler',
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_PAGINATION_CLASS': 'core.pagination.StandardPagination',
     'PAGE_SIZE': 20,
